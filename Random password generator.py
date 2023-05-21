4183fe0a67984cf423f92eb868e12ddbe0927b68
Kanbi Sujal <131600057+Kanbisujal@users.noreply.github.com>
2023-05-21 09:36:31 +0530

    Random password generator.py

--- Random password generator.py
+++ Random password generator.py
@@ -1,14 +1,44 @@
 import random
 import string
 
-# define the length of the password
-length = 10
+def generate_password(length, include_lowercase, include_uppercase, include_digits, include_punctuation):
+    # Define the character set for the password based on the user's choices
+    characters = ""
+    if include_lowercase:
+        characters += string.ascii_lowercase
+    if include_uppercase:
+        characters += string.ascii_uppercase
+    if include_digits:
+        characters += string.digits
+    if include_punctuation:
+        characters += string.punctuation
 
-# define the character set to generate the password from
-characters = string.ascii_letters + string.digits + string.punctuation
+    # Generate a password by randomly selecting characters from the character set
+    password = ''.join(random.choice(characters) for i in range(length))
 
-# generate the password
-password = ''.join(random.choice(characters) for i in range(length))
+    return password
 
-# print the password
-print("Your randomly generated password is:", password)
+# Prompt the user to choose which types of characters should be included in the password
+include_lowercase = input("Include lowercase letters? (y/n): ").lower() == "y"
+include_uppercase = input("Include uppercase letters? (y/n): ").lower() == "y"
+include_digits = input("Include digits? (y/n): ").lower() == "y"
+include_punctuation = input("Include punctuation? (y/n): ").lower() == "y"
+
+# Prompt the user to enter the desired length of the password
+length = int(input("Enter the desired length of the password: "))
+
+while True:
+    # Generate a password based on the user's choices
+    password = generate_password(length, include_lowercase, include_uppercase, include_digits, include_punctuation)
+
+    # Display the password to the user
+    print(f"Your password is: {password}")
+
+    # Ask the user if they like the password
+    choice = input("Do you like this password? (y/n): ")
+
+    if choice.lower() == "y":
+        # Exit the loop if the user likes the password
+        break
+
+print("Thanks for using our password generator!")
